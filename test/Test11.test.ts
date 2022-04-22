1c74dc8456b4274c0de7914e47dc08ba8af53f98
jobmonzonn <jobmonzon@yahoo.com>
2022-04-21 22:41:23 -0300

    Test 11 Funcional

--- test/Test11.test.ts
+++ test/Test11.test.ts
@@ -1,7 +1,6 @@
 import { Estadio } from "../Archivos/Estadio";
 
 
-
 test('11_Cuando_SeCreaUnEstadio_Deberia_Poseer_posicionGeografica_Latitud_Longitud', () => {
     const estadio = new Estadio("Bombonera", 34, 58);
     expect(estadio.Latitud).toBe(34);
