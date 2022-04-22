7e13fc50365eca6ec8e0fb4fc78baaa106ea7a27
jobmonzonn <jobmonzon@yahoo.com>
2022-04-21 22:18:26 -0300

    test 11 rojo

---
+++ test/Test11.test.ts
@@ -0,0 +1,12 @@
+import { Equipo } from "../Archivos/Equipo";
+import { Grupo } from "../Archivos/Grupo";
+import { Estadio } from "../Archivos/Estadio";
+import { Partido } from "../Archivos/Partido";
+import { Manager } from "../Archivos/Manager";
+
+
+test('11_Cuando_SeCreaUnEstadio_Deberia_Poseer_posicionGeografica_Latitud_Longitud', () => {
+    
+    expect(estadio.Latitud).toBe(5);
+    expect(estadio.Longitud).toBe(4);
+});
