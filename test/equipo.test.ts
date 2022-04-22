9baa3b85808ecbaeeadd024c8eaa439fa1e705cb
Dikowiec Sebastian <diko2077@gmail.com>
2022-04-21 22:14:32 -0300

    test 13 no funcional

--- test/equipo.test.ts
+++ test/equipo.test.ts
@@ -750,10 +750,6 @@ test('10_ Los nombres de los equipos no pueden comenzar con un número y no debe
 });
 
 test('13_ Se debe poder hacer que en un partido un equipo no se presente y automáticamente gana el equipo contrario (sin goles a ninguno)', () => {
-    let local = new Equipo("Argentina", "AR");
-    let visitante = new Equipo("Mexico", "MX");
 
-    let partido = new Partido();
-    partido.creacionPartidos2(1, local)
     expect(partido.equipoLocal.puntosTotal).toBe(3);
 });
