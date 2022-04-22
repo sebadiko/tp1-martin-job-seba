2fd3c08e99b763459b8599860eb89aef16502d56
jobmonzonn <jobmonzon@yahoo.com>
2022-04-21 22:19:37 -0300

    Test 11 Refactoring

--- test/Test11.test.ts
+++ test/Test11.test.ts
@@ -6,7 +6,7 @@ import { Manager } from "../Archivos/Manager";
 
 
 test('11_Cuando_SeCreaUnEstadio_Deberia_Poseer_posicionGeografica_Latitud_Longitud', () => {
-    
+    const estadio = new Estadio("Bombonera", 5, 4);
     expect(estadio.Latitud).toBe(5);
     expect(estadio.Longitud).toBe(4);
 });
