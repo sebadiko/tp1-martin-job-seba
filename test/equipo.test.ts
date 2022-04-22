5c08c8c25bf3b7f88ca43963e722885ec6a022c8
Martin <martin.jones.r@gmail.com>
2022-04-21 21:13:16 -0300

    Sebastian: Agregé el test numero 10

--- test/equipo.test.ts
+++ test/equipo.test.ts
@@ -744,7 +744,7 @@ test('901_Cuando_Partidos_Deberia_DevolverLosPartidosDeFinal', () => {
     expect.arrayContaining(man.crearFinal());
 });
 
-test('16_Cuando_Partidos_Deberia_Empezar_Con_3JuecesPartido_3JuecesVar', () => {
+/*test('16_Cuando_Partidos_Deberia_Empezar_Con_3JuecesPartido_3JuecesVar', () => {
     const g = new Grupo("C");
     g.AgregarEquipo("Argentina", "AR");
     g.AgregarEquipo("Mexico", "MX");
@@ -765,4 +765,9 @@ test('11_Cuando_SeCreaUnEstadio_Deberia_Poseer_posicionGeografica_Latitud_Longit
     const estadio = new Estadio("Bombonera", 5, 4);
     expect(estadio.Latitud).toBe(5);
     expect(estadio.Longitud).toBe(4);
+});*/
+
+test('10_ Los nombres de los equipos no pueden comenzar con un número y no deben ser mayor a 50 caracteres', () => {
+    let aux = new Equipo("1Argentina", "AR");
+    expect(aux.getNombre()).toBe(null);
 });
