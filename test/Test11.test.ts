93f649ec0742a74446c45f9cad3c5387c7274c56
Martin <martin.jones.r@gmail.com>
2022-04-21 22:34:06 -0300

    Refactoring

--- test/Test11.test.ts
+++ test/Test11.test.ts
@@ -1,12 +1,9 @@
-import { Equipo } from "../Archivos/Equipo";
-import { Grupo } from "../Archivos/Grupo";
 import { Estadio } from "../Archivos/Estadio";
-import { Partido } from "../Archivos/Partido";
-import { Manager } from "../Archivos/Manager";
+
 
 
 test('11_Cuando_SeCreaUnEstadio_Deberia_Poseer_posicionGeografica_Latitud_Longitud', () => {
-    const estadio = new Estadio("Bombonera", 5, 4);
-    expect(estadio.Latitud).toBe(5);
-    expect(estadio.Longitud).toBe(4);
+    const estadio = new Estadio("Bombonera", 34, 58);
+    expect(estadio.Latitud).toBe(34);
+    expect(estadio.Longitud).toBe(58);
 });
